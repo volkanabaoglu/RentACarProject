906f5ea407b46b5e63426d7401e149a729bfbf72
Volkan <83585653+volkanabaoglu@users.noreply.github.com>
2022-10-02 13:13:32 +0300

    RentAcar

--- Console/Class1.cs
+++ Console/Class1.cs
@@ -1,7 +1,15 @@
-﻿namespace Console
+﻿using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System.Drawing;
+
+namespace Console
 {
     public class Class1
     {
-
+        public void Main(string[] args)
+        {
+          
+            
+        }
     }
 }
